c4fda5e7a75081a1dbf8f92b1e9bbe7116ec7c0b
UninterestinAcc <uninterestinacc@users.noreply.github.com>
2017-11-16 14:14:44 +1100

    fonts rendering

--- src/lib.rs
+++ src/lib.rs
@@ -259,7 +259,9 @@ fn validate(data: &Vec<OHLC>) -> Result<(), &'static str> {
 #[cfg(test)]
 mod tests {
 	use std::fs;
+	use std::io::Write;
 	use super::*;
+	use image::GenericImage;
 
 	#[test]
 	fn render_options_modification() {
@@ -347,5 +349,49 @@ mod tests {
 				} else {
 					Ok(())
 				});
+	}
+
+	// Technically not a test, it just generates the fonts array based on the fonts png.
+	#[test]
+	fn generate_fonts_file() {
+		let img = image::open("consolas-12-ascii-table.png").unwrap();
+
+		// Character sizes are 7 wide, 12 tall
+
+		// ascii table will have 126 elements
+		// First 31 elements of output array are empty
+
+		// Printables are 0x20 - 0x7E
+
+		let mut output = "pub const ASCII_TABLE: [[u8; 84]; 126] = [\n".to_string();
+
+		// 0x00 to 0x20 is filled with blank
+		for _ in 0..(32 + 1) {
+			output += "\t[0u8; 84],\n";
+		}
+
+		for base_y in 2..8 {
+			for base_x in 0..16 {
+				if (base_y == 7 && base_x == 15) || (base_y == 2 && base_x == 0) { continue }
+				output += "\t[\n";
+				// Write character into array.
+				for ptr_y in 0..(12 + 1) {
+					output += "\t\t";
+					for ptr_x in 0..(7 + 1) {
+						let x = (base_x * 7) + ptr_x;
+						let y = (base_y * 12) + ptr_y;
+
+						output += &format!("{},", 255 - img.get_pixel(x, y).data[0]);
+					}
+					output += "\n";
+				}
+				output += "\t],\n";
+			}
+		}
+
+		output += "];";
+
+		let mut f = File::create("src/fonts.rs").unwrap();
+		let _ = f.write(output.as_bytes());
 	}
 }
