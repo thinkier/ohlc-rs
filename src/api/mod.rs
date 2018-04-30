77efc2f4676b8d07548615b5a7ea21e559acd36e
ConcurrentMarxistGC <concurrentmarxistgc@users.noreply.github.com>
2018-04-30 20:34:04 +1000

    ofcourse I forgot to make the data available to the trait implementation

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,7 @@
+use data::OHLC;
 use model::ChartBuffer;
 use OHLCRenderOptions;
 
 pub trait RendererExtension {
-	fn apply(&self, _config: &OHLCRenderOptions, _buffer: &mut ChartBuffer);
+	fn apply(&self, _config: &OHLCRenderOptions, _buffer: &mut ChartBuffer, _data: &[OHLC]);
 }
