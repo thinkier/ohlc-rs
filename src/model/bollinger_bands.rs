2fce8f3db72a0a93d02e2fd86c2faba4b5218faf
ConcurrentMarxistGC <concurrentmarxistgc@users.noreply.github.com>
2018-04-30 21:06:17 +1000

    implemnted required utilities

--- src/model/bollinger_bands.rs
+++ src/model/bollinger_bands.rs
@@ -15,7 +15,30 @@ impl BollingerBand {
 }
 
 impl RendererExtension for BollingerBand {
-	fn apply(&self, config: &OHLCRenderOptions, buffer: &mut ChartBuffer, data: &[OHLC]) {
+	fn apply(&self, config: &OHLCRenderOptions, buffer: &mut ChartBuffer, data: &[OHLC]) {}
+}
 
+fn std_dev(prices: &[f64]) -> f64 {
+	let avg = avg(prices);
+	let mut squared_diff_sum = 0.;
+
+	for price in prices {
+		squared_diff_sum += (avg - price).pow(2);
 	}
+
+	(squared_diff_sum / (prices.len() - 1)).sqrt()
+}
+
+fn avg(prices: &[f64]) -> f64 {
+	let mut sum = 0.;
+
+	for price in prices {
+		sum += price;
+	}
+
+	sum / prices.len()
+}
+
+fn middle_of_ohlc(ohlc: OHLC) -> f64 {
+	(ohlc.h - ohlc.l) / 2 + ohlc.l
 }
