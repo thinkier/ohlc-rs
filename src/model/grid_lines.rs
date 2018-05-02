e2bfb7b30149a67ffc81ddc9a3f07be60369894e
ConcurrentMarxistGC <concurrentmarxistgc@users.noreply.github.com>
2018-05-02 13:04:38 +1000

    rounder price points

--- src/model/grid_lines.rs
+++ src/model/grid_lines.rs
@@ -20,7 +20,7 @@ impl GridLines {
 impl RendererExtension for GridLines {
 	fn apply(&self, buffer: &mut ChartBuffer, _data: &[OHLC]) {
 		{
-			let mut price = buffer.min_price;
+			let mut price = round_start_price(&buffer, self.price_interval);
 			while price <= buffer.max_price {
 				let p1 = buffer.data_to_coords(price, 0);
 				let p2 = buffer.data_to_coords(price, buffer.timeframe);
@@ -36,14 +36,17 @@ impl RendererExtension for GridLines {
 		{
 			let mut time = 0;
 			while time <= buffer.timeframe {
-				let p1 = buffer.data_to_coords(buffer.min_price, time);
+				let p1 = {
+					let point = buffer.data_to_coords(buffer.min_price, time);
+					(point.0, point.1 + 15)
+				};
 				let p2 = buffer.data_to_coords(buffer.max_price, time);
 
 				buffer.line(p1, p2, self.colour);
 
 				if self.label {
 					let elapsed = format!("{}", duration_string((buffer.timeframe - time) as u64));
-					buffer.text((p1.0 - 10, p1.1 + 3), &elapsed, self.colour);
+					buffer.text((p1.0 - 10, p1.1 + 2), &elapsed, self.colour);
 				}
 
 				time += self.time_interval;
@@ -54,4 +57,11 @@ impl RendererExtension for GridLines {
 	fn name(&self) -> String {
 		"CORE_GridLines()".to_string()
 	}
+}
+
+fn round_start_price(buffer: &ChartBuffer, interval: f64) -> f64 {
+	let window = buffer.max_price - buffer.min_price;
+	let count = (window / interval).floor();
+
+	buffer.min_price + interval - (buffer.min_price % interval)
 }
